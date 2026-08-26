69a4d1575b5d4695e60afb80c4158b6cf9d8994a
Paulo Cidro <paulocidro@gmail.com>
2026-08-26 08:46:34 -0300

    feat(api): add products migration, model fillable attributes, and api routes

--- database/migrations/2026_08_26_103509_create_products_table.php
+++ database/migrations/2026_08_26_103509_create_products_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table -> string('name');
+             $table -> string('description');
+             $table -> string('brand');
+             $table -> decimal('price', 10, 2);
+             $table -> integer('stock');
             $table->timestamps();
         });
     }
